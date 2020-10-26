7780cf2235af5a801537994ce923c40a5dec3f32
tobiashrs <66127714+tobiashrs@users.noreply.github.com>
2020-10-26 10:10:54 +0100

    AB#259 build all with top level CMakeLists (#5)
    Co-authored-by: Tobias Horst <tobias.horst-v97@rub.de>

--- make.sh
+++
@@ -1,8 +0,0 @@
-# using a top level cmake with add_subdirectory(..) introduces issues with the config tool regarding path, for now use this script to build them
-ROOT_DIR=$(pwd)
-echo "build emoji-svc"
-mkdir -p $ROOT_DIR/emojivoto-emoji-svc/build && cd $ROOT_DIR/emojivoto-emoji-svc/build && cmake .. && make
-echo "build voting-svc"
-mkdir -p $ROOT_DIR/emojivoto-voting-svc/build && cd $ROOT_DIR/emojivoto-voting-svc/build && cmake .. && make
-echo "build web"
-mkdir -p $ROOT_DIR/emojivoto-web/build && cd $ROOT_DIR/emojivoto-web/build && cmake .. && make
